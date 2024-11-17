753b2242c9231161405061f96a07e140cebbc8ec
DGoiana <diogogoianam@gmail.com>
2024-11-17 19:31:29 +0000

    adding resume and contact

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+import resume from "../assets/resume.pdf"
+
 const Header = () => {
    return (
       <section className="py-5 px-16 text-black flex justify-between font-semibold">
@@ -5,10 +7,10 @@ const Header = () => {
             <a href="/">&lt;/me&gt;</a>
          </div>
          <div className="flex gap-10">
-            <a href="/about">about</a>
-            <a href="/work">work</a>
-            <a>resume</a>
-            <a>contact</a>
+            <a href="/about" className="hover:underline">about</a>
+            <a href="/work" className="hover:underline">work</a>
+            <a href={resume} download className="hover:underline">resume</a>
+            <a className="hover:underline" href="mailto:diogogoianam@gmail.com">contact</a>
          </div>
       </section >
    )
