94154caf4a7f720deae83a7d9fb800cf2c8ca46b
DGoiana <diogogoianam@gmail.com>
2025-01-10 22:02:52 +0000

    adding analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Homepage from './pages/Homepage'
-import AboutPage from './pages/AboutPage'
-import WorkPage from './pages/WorkPage'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Homepage from "./pages/Homepage";
+import AboutPage from "./pages/AboutPage";
+import WorkPage from "./pages/WorkPage";
+import { Analytics } from "@vercel/analytics/next";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Homepage />}></Route>
-        <Route path='/about' element={<AboutPage />}></Route>
-        <Route path='/work' element={<WorkPage />}></Route>
+        <Route path="/" element={<Homepage />}></Route>
+        <Route path="/about" element={<AboutPage />}></Route>
+        <Route path="/work" element={<WorkPage />}></Route>
       </Routes>
+      <Analytics />
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
